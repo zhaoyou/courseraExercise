3c869a4b474e5cb55907f82ca70406030fd379a1
zhaoyou <zhaoyou.xt@gmail.com>
2013-03-07 09:54:17 +0800

    add UF_weight

--- algorithms/week1/UF_Weight.java
+++ algorithms/week1/UF_Weight.java
@@ -1,13 +1,17 @@
 /**
  * zhaoyou.xt@gmail.com (zhaoyou)
  */
-public class UF2 {
+public class UF_Weight {
   int [] id;
+  int [] size;
 
-  public UF2(int n) {
+  public UF_Weight(int n) {
     id = new int[n];
+    size = new int[n];
+
     for (int i = 0; i < n; i++) {
       id[i] = i;
+      size[i] = 1;
     }
   }
 
@@ -16,31 +20,34 @@ public class UF2 {
   }
 
   public void union(int p, int q) {
-    //int pid = id[p];
-    //int qid = id[q];
 
-    //for (int i = 0; i < id.length; i++) {
-    //  if (id[i] == pid) {
-    //    id[i] = qid;
-    //  }
-    //}
     int i = root(p);
     int j = root(q);
 
-    id[i] = j;
+    if (i == j) return;
+
+    if (size[i] < size[j]) {
+      id[i] = j;
+      size[j] += size[i];
+    } else {
+      id[j] = i;
+      size[i] += size[j];
+    }
   }
 
-  public int root(int p) {
-    while(p != id[p]) {
-      p = id[p];
+  public int root(int i) {
+    while(i != id[i]) {
+      //StdOut.println("i: " + id[i] + "id[id[i]]: " + id[id[i]]);
+      id[i] = id[id[i]];
+      i = id[i];
     }
-    return p;
+    return i;
   }
 
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
     int n = StdIn.readInt();
-    UF2 uf = new UF2(n);
+    UF_Weight uf = new UF_Weight(n);
     while(!StdIn.isEmpty()) {
       int p =  StdIn.readInt();
       int q =  StdIn.readInt();
@@ -55,5 +62,9 @@ public class UF2 {
     for (int i = 0; i < uf.id.length; i ++) {
       StdOut.println(uf.id[i]);
     }
+    //StdOut.println("***************");
+    //for (int i = 0; i < uf.size.length; i ++) {
+    //  StdOut.println(uf.size[i]);
+    //}
   }
 }
